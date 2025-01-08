9b8d475ad022e6b8028935b0b2eaea4aadea764f
Onkar Sathe <onkarsathe96k@gmail.com>
2025-01-08 22:05:43 +0530

    Set UP

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+let port  = 8080;
+
+//setting up the server
+app.listen(8080,()=>{
+    console.log("Server is listening ",port);
+});
+
+//setting up the database
+let mongoUrl = 'mongodb://127.0.0.1:27017/test';    
+async function main(){
+    await mongoose.connect(mongoUrl);
+}
+
+main().then((res)=>{
+    console.log("Conneted to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+//root API
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root!");
+})
