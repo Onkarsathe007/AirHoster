2005c027eec12941aebd1c36e9165252b19abde4
Onkar Sathe <onkarsathe96k@gmail.com>
2025-01-09 22:28:01 +0530

    new route | show route

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 const Listing = require("./models/listing.js"); //accessing the model
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const path = require("path");
+app.set("view engine","ejs");
+
+app.set("views",path.join(__dirname,"views"));
+
 let port  = 8080;
 
 //setting up the server
@@ -28,3 +36,61 @@ main().then((res)=>{
 app.get("/",(req,res)=>{
     res.send("Hi, I am root!");
 })
+
+// //test listing 
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing  = new Listing({
+//         title : "My new villa",
+//         description : "By the beach",
+//         price : 999000,
+//         location : "Ratnagiri, Maharashtra",
+//         country : "India",
+//     });
+//     await sampleListing.save().then((result)=>{
+//     console.log(result," saved !");
+//     res.send("Successful testing")
+//     });
+// });
+
+app.get("/listings",async (req,res)=>{
+    //accessing all the data from mongoDB to 'allListings' Variable.
+    let allListings =  await Listing.find({});
+    res.render("./listings/index.ejs",{allListings : allListings});
+})
+
+// new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//new route | Post request to add the data |
+app.post("/listings/new", (req, res) => {
+    const { title, description, imgLink, price,location, country } = req.body;
+    console.log(req.body.listing); // Logs the entire listing object
+    // console.log(title, description, imgLink, price, country); // Logs individual properties
+    // res.send("Form submitted!");
+    Listing.insertMany([
+        {
+            title: title,
+            description: description,
+            image: imgLink,
+            price: price,
+            location : location,
+            country : country,
+        }
+    ]).then((res)=>{
+        console.log("data saved successfully ");
+    }).catch((err)=>{
+        console.log("Error Occured ");
+    })
+    res.redirect("/listings")
+});
+
+
+//show route 
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const listData =  await Listing.findById(id);
+    console.log(listData);
+    res.render("listings/show.ejs",{ listing : listData})
+});
