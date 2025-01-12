3a95bb13373b7745b97b518e157ad5728ed874e2
Onkar Sathe <onkarsathe96k@gmail.com>
2025-01-12 11:49:15 +0530

    Add EJS-Mate templating and style the page

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.engine("ejs",ejsMate)
 
 app.set("views",path.join(__dirname,"views"));
 
+app.use(express.static(path.join(__dirname,"public")));
 let port  = 8080;
 
 //setting up the server
--- models/listing.js
+++ models/listing.js
@@ -8,13 +8,17 @@ const listingSchema = new Schema({
         required : true,
     },
     description : String,
-    image : {
-        type : String,
-         //default image if no image
-        default : "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set  : (v)=>{ v === "" ? "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v}, 
-         //using an ternary operator.
-    }, 
+    image: {
+        type: Object,  // Change from String to Object
+        default: {
+            filename: '',
+            url: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        },
+        set: (v) => {
+            // Ensure the value is an object with a url property.
+            return typeof v === 'string' ? { url: v } : v;
+        }
+    },
     price : Number,
     location : String,
     country : String,
